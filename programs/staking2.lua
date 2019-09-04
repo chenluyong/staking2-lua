602bbfcab6390ebf95ecd4cb9f160c523fe90df1
root <727057301@qq.com>
2019-09-04 09:18:24 +0000

    add: add main function

--- programs/staking2.lua
+++ programs/staking2.lua
@@ -53,61 +53,64 @@ function rds_set(_k, _v, _time)
     return true
 end
 
+local function main()
+    local ok, err = pcall(function()
+        -- get request uri
+        local request_all_uri = ngx.var.request_uri
+        local request_uri = ngx.var.uri
 
-local ok, err = pcall(function()
-    -- get request uri
-    local request_all_uri = ngx.var.request_uri
-    local request_uri = ngx.var.uri
+        --[[
+        get info
+        --]]
+        -- get cache
+        local ret = rds_get(request_all_uri)
+        if ret ~= nil and ret ~= ngx.null then
+            return cjson.decode(ret)
+        end
 
-    --[[
-    get info
-    --]]
-    -- get cache
-    local ret = rds_get(request_all_uri)
-    if ret ~= nil and ret ~= ngx.null then
-        return cjson.decode(ret)
+        -- get lua path
+        local path = string.gsub(request_uri, "/", ".")
+        path = string.sub(path,2)
+
+        -- call
+        local lua = require(path)
+        local ret_table = lua.main()
+
+        --[[
+        check
+        --]]
+
+        return ret_table
+    end
+    --, function() RET.tracebak = debug.traceback() end, 133
+    )
+
+    -- alias
+    local ret_table = err 
+
+    if not ok then
+        RET.error = err
+        -- RET.tracebak = debug.traceback()
+        if not RET.code then
+            RET.code = debug.getinfo(1).currentline
+        end
+    else
+        -- merge tables
+        for k,v in pairs(ret_table) do
+            RET[k] = v
+        end
+
+        -- cache result
+        if not RET.error and not RET.warning then
+            rds_set(ngx.var.request_uri, cjson.encode(RET))
+        end
     end
 
-    -- get lua path
-    local path = string.gsub(request_uri, "/", ".")
-    path = string.sub(path,2)
 
-    -- call
-    local lua = require(path)
-    local ret_table = lua.main()
-
-    --[[
-    check
-    --]]
-
-    return ret_table
+    -- put it into the connection poll
+    rds:set_keepalive(10000,100)
+    -- return
+    ngx.say(cjson.encode(RET))
 end
---, function() RET.tracebak = debug.traceback() end, 133
-)
 
--- alias
-local ret_table = err 
-
-if not ok then
-    RET.error = err
-    -- RET.tracebak = debug.traceback()
-    if not RET.code then
-        RET.code = debug.getinfo(1).currentline
-    end
-else
-    -- merge tables
-    for k,v in pairs(ret_table) do
-        RET[k] = v
-    end
-
-    -- cache result
-    if not RET.error and not RET.warning then
-        rds_set(ngx.var.request_uri, cjson.encode(RET))
-    end
-end
-
-
--- put it into the connection poll
-rds:set_keepalive(10000,100)
--- return
-ngx.say(cjson.encode(RET))
+main()
