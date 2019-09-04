e65be1058ec436aa7b987129fb123819da716a97
root <727057301@qq.com>
2019-09-04 06:21:58 +0000

    mod: merge content

--- config.lua
+++ config.lua
@@ -17,6 +17,9 @@ local PATCH_VERSION = _M.git_commit_count()
 _M.VERSION = MAJOR_VERSION .. "." .. MINOR_VERSION .. "." .. PATCH_VERSION
 
 
+_M.CAMO_UA = {['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36'}
+
+
 
 
 _M.REDIS = {
