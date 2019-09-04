4a34096a004378ce9a9d10d0e6f60788385a0bf8
root <727057301@qq.com>
2019-09-04 06:04:45 +0000

    add: global config file

--- config.lua
+++ config.lua
@@ -2,20 +2,42 @@ local _M = {}
 
 
 -- get git commit count
-function git_commit_count()
+function _M.git_commit_count()
     local sh = "git rev-list --all --count"
     local t = io.popen(sh)
     local count = t:read("*all")
-    return tonumber(count)
+    return tonumber(count) or 0
 end
 
 --
 local MAJOR_VERSION = 1
 local MINOR_VERSION = 0
-local PATCH_VERSION = git_commit_count() 
+local PATCH_VERSION = _M.git_commit_count()
 
 _M.VERSION = MAJOR_VERSION .. "." .. MINOR_VERSION .. "." .. PATCH_VERSION
 
+
+
+
+_M.REDIS = {
+    
+    ip = "192.168.1.93",
+    port = 6379,
+    timeout = 1000,
+    account_list = {
+        wanchain = 3600,
+        default  = 1200 -- default 20 minutes timeout
+    },
+    nodes_list = {
+        wanchain = 0, -- no cache, real-time
+        default = 86400
+    }
+}
+
+
+-- DEBUG
+--print(_M.VERSION)
+--print(_M.REDIS['account_list']['wanchain'])
 
 return _M
 
