cd426109fecf8aff435fd57a8d4d3697f9d113bc
root <727057301@qq.com>
2019-09-04 10:29:25 +0000

    mov: clean

--- plugins/nodes/chainx.lua
+++
@@ -1,6 +0,0 @@
-local cjson = require "cjson"
-local redis = require "resty.iredis"
-local rds = redis:new()
-local http = require "resty.ihttp"
-local httpc = http:new()
-local util = require "util"
