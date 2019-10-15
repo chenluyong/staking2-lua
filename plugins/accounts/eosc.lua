f31eeda169c0e33b324b9843acbf73787c18c4b2
root <727057301@qq.com>
2019-10-15 08:43:08 +0000

    add: recently the income

--- plugins/accounts/eosc.lua
+++ plugins/accounts/eosc.lua
@@ -4,6 +4,9 @@ local httpc = http:new()
 local util = require "staking2.util"
 local config = require "config"
 
+local log = ngx.log
+local ERR = ngx.ERR
+
 local _M = {}
 
 local string = string
@@ -24,13 +27,63 @@ local function split(str, sep)
     return t
 end
 
+
+local function get_24_hour(account)
+    http = require "resty.http"
+    httpc = http.new()
+_M.code = debug.getinfo(1).currentline
+    local req_uri = "https://explorer.eosforce.io/web/get_action"
+    local request_body = cjson.encode({
+              account_name = account, 
+              limit = 10000000000, 
+              offset = 1, 
+              pos = 0, 
+              pre_id = -1})
+    local res, err = httpc:request_uri(req_uri, {
+        method = "POST",
+        headers = {
+            ['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36',
+           ['Content-Type'] = "application/json"
+        },
+--        headers = config.CAMO_UA, 
+        body = request_body 
+    })
+
+_M.code = debug.getinfo(1).currentline
+
+    if not res then
+        return {status = 1, error = "request "..req_uri.."failed", code = debug.getinfo(1).currentline}
+    end
+
+    local ret = cjson.decode(res.body)
+    local time_with_24_hour = os.time() - (24 * 60 * 60)
+_M.code = debug.getinfo(1).currentline
+
+    if ret then
+        local amount_24_hour = 0
+        for _,action in pairs(ret.data) do
+            local _, _, y, m, d, hour, min, sec = string.find(action.block_time, "(%d+)-(%d+)-(%d+)T(%d+):(%d+):(%d+)")
+            local timestamp = os.time({year=y, month = m, day = d, hour = _hour, min = _min, sec = _sec});
+            if time_with_24_hour < timestamp then
+                if action.name == "transfer" and action.token_type == "EOS" then
+                    local to = action.to
+                    if to == account then
+                        amount_24_hour = amount_24_hour + tonumber(string.sub(action.quantity,1,-4))
+                    end
+                end
+            end
+        end
+       return amount_24_hour
+    end
+end
+
 local function normalize(str)
     local val = split(str)
     return tonumber(val[1])
 end
 
 function _M.main()
-
+--if true then return { aaa = "aaa"} end
     local addr = args.acc
     if not addr then
     --    log(ERR, "ERR not bystack address provided.")
@@ -112,6 +165,8 @@ function _M.main()
         end
 
     end
+    RET.recentFunding = get_24_hour(addr)
+_M.code = 0
     return RET
 end
 
