c9a80ee09ef2fb9876564345a400611ab4a9f8fa
root <727057301@qq.com>
2019-10-17 06:09:34 +0000

    fix: modify the roi field to number type

--- plugins/nodes/bystack.lua
+++ plugins/nodes/bystack.lua
@@ -86,7 +86,7 @@ _M.code = debug.getinfo(1).currentline
                     nodeinfo[name].type = node.type
                     --nodeinfo[name].ip = node.address
                     nodeinfo[name].pub_key = node.pub_key
-                    nodeinfo[name].roi = (node.expected_return)
+                    nodeinfo[name].roi = tonumber(node.expected_return)
                     nodeinfo[name].vote_percent = tonumber(string.format("%.4f", node.vote_count / netTotalVote)) * 100
                 else
 --                    log(ERR, ">>name: '"..name.."' not found")
