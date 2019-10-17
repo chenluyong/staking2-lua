ade32a4608aee0d7fc3d4cf9c8147f6a1bedd9e6
root <727057301@qq.com>
2019-10-17 03:34:01 +0000

    add: recent funding.

--- plugins/accounts/lambda.lua
+++ plugins/accounts/lambda.lua
@@ -9,7 +9,7 @@ local function get_24_hour(account)
     http = require "resty.http"
     httpc = http.new()
 
-    local req_uri = string.format("https://www.iostabc.com/api/account/%s/actions?page=1&size=20", account)
+    local req_uri = string.format("https://explorer.lambdastorage.com/api/tx/txListByAddress?address=%s&pageNum=1&showNum=10&msgType=all", account)
     local res, err = httpc:request_uri(req_uri, {
         method = "GET",
         headers = config.CAMO_UA
@@ -17,26 +17,34 @@ local function get_24_hour(account)
     if not res then
         return {status = 1, error = "request "..req_uri.."failed", code = debug.getinfo(1).currentline}
     end
-     
     local ret = cjson.decode(res.body)
     local time_with_24_hour = os.time() - (24 * 60 * 60)
 
+_M.code = debug.getinfo(1).currentline
     if ret then
         local amount_24_hour = 0
-        for _,action in pairs(ret.actions) do
-            local _, _, y, m, d, hour, min, sec = string.find(action.created_at, "(%d+)-(%d+)-(%d+)T(%d+):(%d+):(%d+)")
+        for _,action in pairs(ret.data.tx_list) do
+        for _, tx in pairs(action.txs) do
+            local _, _, y, m, d, hour, min, sec = string.find(tx.create_time, "(%d+)-(%d+)-(%d+)T(%d+):(%d+):(%d+)")
             local timestamp = os.time({year=y, month = m, day = d, hour = _hour, min = _min, sec = _sec});
+            local ulamb_amount = 0
             if time_with_24_hour < timestamp then
-                local action_name = action.action_name
-                local data_obj = cjson.decode(action.data)
+                local action_name = tx.action
+                if true then 
+                    local b,e = string.find(tx.amount, 'ulamb')
+                    if b and e then
+                        ulamb_amount = tonumber(string.sub(tx.amount,1,b-1)) / 1000000
+                    end
+                end
 
-                if action_name == "transfer" and data_obj[1] == "iost" then
-                    local to = action.to
+                if action_name == "withdraw_delegator_reward" or action_name == "send" then
+                    local to = tx.to
                     if to == account then
-                        amount_24_hour = amount_24_hour + tonumber(data_obj[4])
+                        amount_24_hour = amount_24_hour + tonumber(ulamb_amount)
                     end
                 end
             end
+        end
         end
        return amount_24_hour
     end
@@ -100,8 +108,8 @@ _M.code = debug.getinfo(1).currentline
         RET.balance = RET.balanceTotal
 
     end
+    RET.recentFunding = get_24_hour(acc)
 _M.code = 0
---    RET.recentFunding = get_24_hour(acc)
     RET.status = 0
     return RET
 end
